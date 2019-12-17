8325b25b83082fad7d393a484d8afcb3b375fb33
Erik <erickopcha@gmail.com>
2019-12-17 23:57:40 +0200

    fixes modal window

--- source/js/modal.js
+++ source/js/modal.js
@@ -1,10 +1,12 @@
-var modal = document.querySelector('#modal-add');
+var modal = document.querySelectorAll('.modal-add');
 var modalShow = document.querySelector('.modal');
 
-modal.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  modalShow.classList.add("modal-show");
-});
+for (var i = 0 ; i < modal.length; i++) {
+  modal[i].addEventListener("click", function(evt) {
+    evt.preventDefault();
+    modalShow.classList.add("modal-show");
+  });
+}
 
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
