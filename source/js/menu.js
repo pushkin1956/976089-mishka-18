51059e617cb6c70c02e0e124d733597f927250cb
Erik <erickopcha@gmail.com>
2019-12-17 19:34:59 +0200

    add body.no-js class

--- source/js/menu.js
+++ source/js/menu.js
@@ -2,6 +2,7 @@ var navMenu = document.querySelector('.menu');
 var navBtn = document.querySelector('.menu__btn');
 
 navMenu.classList.remove('menu__no-js');
+document.body.classList.remove('no-js');
 
 navBtn.addEventListener('click', function() {
   if (navMenu.classList.contains('menu__closed')) {
