135a5be4d01702258a92fd2fb8d3f8cb4fafc67b
Keks <keks@htmlacademy.ru>
2019-12-24 23:17:48 +0300

    :heavy_check_mark: Сборка #18

---
+++ 18/js/map.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+  var mapEl = document.querySelector('#map');
+  var mapJS = document.querySelector('.contacts__map');
+
+  mapEl.classList.remove('contacts__map-js--dn');
+  mapJS.classList.add('contacts__map-js--dn');
+
+  if (mapEl) {
+    ymaps.ready(init); // активируем карту после загрузки страницы
+  }
+
+  var map;
+  function init() {
+    map = new ymaps.Map("map", { // в кавычках id элемента куда загружается карта
+      center: [59.9387165, 30.32304739999995],
+      zoom: 18, // координаты центра фрагмента карты
+      controls: []
+    });
+    var placemark = new ymaps.Placemark([59.9387165, 30.32304739999995], {}, { // координаты метки
+      iconLayout: "default#image",
+      iconImageHref: "../img/icon-map-pin.svg", // иконка метки
+      iconImageSize: [82, 82] // размер метки
+    });
+  map.geoObjects.add(placemark);
+  }
+});
