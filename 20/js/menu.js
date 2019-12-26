46a32bdf00c359b51fd4dda6654368ffcae7a3ab
Keks <keks@htmlacademy.ru>
2019-12-26 23:09:04 +0300

    :heavy_check_mark: Сборка #20

---
+++ 20/js/menu.js
@@ -0,0 +1,9 @@
+var navMenu = document.querySelector('.menu');
+var navBtn = document.querySelector('.menu__btn');
+
+navMenu.classList.remove('menu__no-js');
+document.body.classList.remove('no-js');
+
+navBtn.addEventListener('click', function() {
+  navMenu.classList.toggle('menu__opened');
+});
