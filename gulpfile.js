435b0b988df3749da88b0ebaec6c0b498ae736a7
Erik <erickopcha@gmail.com>
2019-12-27 12:08:24 +0200

    fixes error

--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,7 @@ gulp.task("clean", function () {
 
 gulp.task("copy", function () {
   return gulp.src([
-    "source/fonts/**/*.{woff,woff2}", 
+    "source/fonts/**/*.{woff,woff2}",
     "source/img/**/*.{jpg,png,webp}",
     "source/js/**"
   ], {
